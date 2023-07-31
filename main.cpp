0a6236c5f8718d12648932dc55cafdeb7db0f3a1
clement32112 <totoobieke@gmail.com>
2023-07-31 18:56:11 +0100

    VERSION 0.0.0

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
+int main(){ 
+ 
+    sf::Window window;
+   try{
+    window.create(sf::VideoMode(800, 600), "My window");
+   }
+    catch(std::exception e){
+        std::cerr << e.what() <<std::endl;
+    }
 
-int main()
-{
-    std::cout << "Hello world";
-}
+       while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+ 
+    }
