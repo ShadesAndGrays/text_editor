4378027f7b45d0ad6202383079e315be74d83ebf
clement32112 <totoobieke@gmail.com>
2023-07-31 21:58:56 +0100

    0.2.0 refactor

--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
-/* #include <iostream>
+#include <iostream>
 #include "SFML/Graphics.hpp"
 int main(){ 
  
-    sf::Window window;
-   try{
+    sf::RenderWindow window;
     window.create(sf::VideoMode(800, 600), "My window");
-   }
-    catch(std::exception e){
-        std::cerr << e.what() <<std::endl;
-    }
+   
+    sf::Text Text;
+        Text.setFillColor(sf::Color::Black);
+        //Text.setPosition(sf::Vector2f(0.0,0.0));
+        sf::Font Font;
+        Font.loadFromFile("font/ARIAL.TTF");
+        Text.setFont(Font);
+        std::string inputText = "";
 
-       while (window.isOpen())
+        Text.setString(inputText);
+    while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
+        
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::TextEntered)
+            {
+                if (event.text.unicode >= 128)
+                    continue;
+                if (event.text.unicode  == 8 && !inputText.empty()){
+                    inputText.pop_back();
+                    }
+                else if(event.text.unicode  == 13){inputText += '\n';}
+                else                {
+                    inputText += static_cast<char>(event.text.unicode);
+                }
+            Text.setString(inputText);
+            }
         }
+            window.clear(sf::Color::White);
+            window.draw(Text);
+            window.display();
+
     }
  
-    } */
+    }
 
-    #include <SFML/Graphics.hpp>
+/* #include <SFML/Graphics.hpp>
 #include <string>
 
 int main()
@@ -45,9 +68,15 @@ int main()
     text.setFont(font);
     text.setCharacterSize(24);
     text.setFillColor(sf::Color::White);
+    
 
     std::string inputText;
-
+    for (int i = 31; i < 128; i++){
+        if (i %10 == 0)
+           inputText += '\n';
+    inputText += std::to_string(i)+":"+ static_cast<char>(i) +" ";
+    }
+    text.setString(inputText);
     while (window.isOpen())
     {
         sf::Event event;
@@ -77,7 +106,7 @@ int main()
                     }
 
                     // Update the text displayed on the screen
-                    text.setString(inputText);
+                    text.setString(inputText+"_");
                 }
             }
         }
@@ -89,3 +118,4 @@ int main()
 
     return 0;
 }
+ */
